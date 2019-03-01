563fc6daf44df0a7f1a030073d3f9697d7d572ae
AleksNikos <41049320+AleksNikos@users.noreply.github.com>
2019-03-01 19:23:35 +0200

    Create toggleAttr.js

---
+++ toggleAttr.js
@@ -0,0 +1,11 @@
+$.fn.toggleAttr = function(attr, attr1, attr2) {
+  return this.each(function() {
+    var self = $(this);
+    if (self.attr(attr) == attr1)
+      self.attr(attr, attr2);
+    else
+      self.attr(attr, attr1);
+  });
+};
+//primer
+$(this).toggleAttr('type', 'text', 'password');
